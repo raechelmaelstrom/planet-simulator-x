f4478f9bdad36eeb5da00caf37d9d1929d6f5f97
Raechel Maelstrom <raechelmaelstrom@gmail.com>
2026-02-20 15:56:47 -0700

    Add terrain smoothing.

--- planetmodel.py
+++ planetmodel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 import constants
 
@@ -6,6 +7,8 @@ class PlanetModel:
     def __init__(self):
         self.rng = np.random.default_rng()
         self.world = self.rng.integers(low=0, high=255, size=self.size())
+
+        self.world = scipy.ndimage.gaussian_filter(self.world, .9)
 
     def size(self):
         return (constants.PLANET_TILES_X, constants.PLANET_TILES_Y)
